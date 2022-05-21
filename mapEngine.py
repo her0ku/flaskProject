6b14dcc59e3166875841b8baf0046dcaeec8d042
her0ku <drdrink41@gmail.com>
2022-05-21 07:03:56 +0300

    add Login, Registration future -> map with 2gis

---
+++ mapEngine.py
@@ -0,0 +1,35 @@
+import openrouteservice
+from openrouteservice import convert
+import json
+import requests
+import folium
+import os
+
+
+def calculate_route(route1, route2):
+    #coords = ((37.724291, 55.797326), (37.723913, 55.798299))
+    #coords = ((37.763445, 55.805075), (37.763336, 55.806007))
+    path = os.path.join(os.path.abspath(os.path.dirname('templates/')), 'results.html')
+    os.remove(path)
+    coords = (route1, route2)
+    lat = coords[0][0]
+    len = coords[0][1]
+    client = openrouteservice.Client(key='5b3ce3597851110001cf62484e0839f873644b94bd8b6d803fafa16c')
+    routes = client.directions(coords, profile='wheelchair', optimize_waypoints=True, format='geojson')
+    map_directions = folium.Map(location=[len, lat], zoom_start=17)
+    folium.GeoJson(routes, name='route').add_to(map_directions)
+    folium.LayerControl().add_to(map_directions)
+    map_directions.save('templates/results.html')
+
+
+def add_custom_mark():
+    map_code = folium.Map(location=[55.797326, 37.724291], zoom_start=17)
+    folium.Marker(location=(55.797326, 37.724291), icon=folium.Icon(icon='building', color='green', prefix='fa'),
+                  popup=folium.Popup('aaaa')).add_to(map_code)
+    map_code.save('templates/marks.html')
+#es = requests.get('https://maps.openrouteservice.org/#/directions/%D0%9C%D1%8F%D1%81%D0%BD%D0%BE%D0%B2%D1%8A,%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0,MS,%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D1%8F/%D0%B4%D0%BE%D0%BC%2010,%D0%BA%D0%BE%D1%80%D0%BF%D1%83%D1%81%201,%D1%81%D1%82%D1%80%D0%BE%D0%B5%D0%BD%D0%B8%D0%B5%201%20%D0%91%D0%BE%D0%BB%D1%8C%D1%88%D0%B0%D1%8F%20%D0%A7%D0%B5%D1%80%D0%BA%D0%B8%D0%B7%D0%BE%D0%B2%D1%81%D0%BA%D0%B0%D1%8F%20%D0%A3%D0%BB%D0%B8%D1%86%D0%B0,%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0,MS,%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D1%8F')
+#print(res.text)
+#json_res = res.json()
+#for i in json_res['features']:
+#    for n in i['geometry']['coordinates']:
+#        print(n)
---
+++ sqlLogin.py
@@ -0,0 +1,23 @@
+import init_db
+
+
+def create_user(first_name, last_name, mail, date_birth, phone, nickname, bpassword, date_now, country, city, age):
+    conn = init_db.get_conn()
+    cur = conn.cursor()
+    cur.execute('SELECT email, user_name FROM users WHERE email = %s OR user_name = %s', (mail, nickname, ))
+    if cur.fetchone() is None:
+        cur.execute(
+            'INSERT INTO users (first_name, last_name, email, date_birth, phone, user_name, password, created_date, country, city, age ) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)',
+            (first_name, last_name, mail, date_birth, phone, nickname, bpassword, date_now, country, city, age))
+        conn.commit()
+    else:
+        return False
+    cur.close()
+    conn.close()
+
+
+def find_user_by_mail(mail):
+    conn = init_db.get_conn()
+    cur = conn.cursor()
+    cur.execute('SELECT email, password FROM users WHERE email = %s', (mail, ))
+    return cur.fetchone()[1]
