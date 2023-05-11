be3af72fdc5dffb045e27dfb3092bddf8e2a1434
Daniel Maliy <46974529+her0ku@users.noreply.github.com>
2023-05-11 16:01:56 +0300

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -4,8 +4,8 @@ import psycopg2
 
 def get_conn():
     conn = psycopg2.connect(
-            host='ec2-34-247-72-29.eu-west-1.compute.amazonaws.com',
-            database="d466as9buoni53",
-            user='rzrcatwrsoxksq',
-            password='512ffb9447de5be9e3b0ffe93d9541bc0d4ee3ccdecc289f3e8112a4c0741bec')
+            host='',
+            database="",
+            user='',
+            password='')
     return conn
